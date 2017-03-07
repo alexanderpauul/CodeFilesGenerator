67e32c1e68ead273717426a35fe38fa84294fbc9
alexanderpauul <alexanderpauul@hotmail.com>
2017-03-07 14:36:16 -0400

    Downloable files deleted

--- alpaul_gls/Content/Downloadable/UADMISIONES20170307143209/SQL/Academico_Carrera_SelectAll.sql
+++
@@ -1,7 +0,0 @@
-USE UADMISIONES
-GO
-
-CREATE PROCEDURE Academico.Carreras_GetAll
-AS
-	SELECT CarreraId, Codigo, DscCarrera, TCarreraId, UsuarioId, FechaRegistro, Modificacion, Identificador, EstadoId, Diccionario_Key 
-	  FROM Academico.Carreras
